a8473d2333edca4820508f3ca877abcf772c4e7a
2016JTM2093 <sharma.poojapooja76@gmail.com>
2016-09-12 17:06:52 +0530

    final updation

--- ps2.py
+++ ps2.py
@@ -7,5 +7,15 @@ import math
 count=0
 cnt=0
 for num in range(1000):
-
-####### write your code here ##########
+  
+    X,Y=(random.random()*2- 1, random.random()*2-1)
+	  d=int(abs(math.sqrt(X**2 + Y**2)))
+	  if (d==0):
+	  	cnt+=1		
+	  else:
+  		continue
+	
+			
+k=float(float(cnt)/1000)
+k=k*100
+print k####### write your code here ##########
