0d12578b2acce8f9689e2b6af7b2366a1feaebec
2016JTM2093 <sharma.poojapooja76@gmail.com>
2016-09-12 17:05:29 +0530

    file updated

--- ps2.py
+++ ps2.py
@@ -1,3 +1,11 @@
 ###### this is the second .py file ###########
+#!/usr/bin/python -tt
+#Code for Problem Statement 2
+
+import random
+import math
+count=0
+cnt=0
+for num in range(1000):
 
 ####### write your code here ##########
