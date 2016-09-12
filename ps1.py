2abb8aed1170b1876d729babebfeb95d782973ce
2016JTM2093 <sharma.poojapooja76@gmail.com>
2016-09-12 15:16:27 +0530

    The file is updated

--- ps1.py
+++ ps1.py
@@ -1,3 +1,24 @@
 ###### this is the first .py file ###########
+#!/bin/python
 
+import sys
+import collections
+
+print sys.argv[1:]
+dict = {}
+
+for letter in sys.argv[1:]:
+	if letter not in dict.keys():
+		dict[letter] = 1
+		
+	else: 
+		dict[letter] = dict[letter] + 1
+print dict
+
+def top3words(dict):
+    freq = collections.Counter(dict)
+    return [elem for elem, _ in freq.most_common(3)]
+
+x = top3words(dict)
+print x
 ####### write your code here ##########
